2ef326c3b6c3cfedd841d0a60bf6f5fe8676d099
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2025-02-05 19:44:49 +0200

    l

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import sys
 import os
+import uvicorn
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=3500, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", reload=True)
