1fe8262e6f4c4b640ef32f668e326cb3f69496e4
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2025-02-06 01:31:34 +0200

    minors

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ import sys
 import os
 import uvicorn
 
+# docker build -t pcpartsearcher .
+# docker run -p 8000:8000 pcpartsearcher
+
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 if __name__ == "__main__":
